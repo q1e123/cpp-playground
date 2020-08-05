de860764967de174fba9af4744a99894cc0054bb
Robert <robertnicolaesolca@protonmail.com>
2020-08-05 18:45:42 +0300

    changed main

--- 15_game_of_life/src/main.cpp
+++ 15_game_of_life/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "world.h"
 #include "shape.h"
@@ -28,20 +29,29 @@ int main(int argc, char **argv)
 	if(argc == 5 && std::string(argv[4]) == "-a"){
 		animation = true;
 	}
+
 	World world(epochs, heigth, width);
+
 	Block *block = new Block(0,0);
-	Boat *boat = new Boat(3,3);
-	Blinker *blinker = new Blinker(1, 7);
-	Beacon *beacon = new Beacon(0, 0);
+	Boat *boat = new Boat(15,3);
+	Blinker *blinker = new Blinker(22, 7);
+	Beacon *beacon = new Beacon(4, 5);
+	Pulsar *pulsar = new Pulsar(30,20);
+	Pentadecathlon *pentadecathlon = new Pentadecathlon(35, 5);
+	Glider *glider = new Glider(7,7);
 
-	Pulsar *pulsar = new Pulsar(0,0);
+	std::vector<Shape*> shapes;
+	shapes.push_back(block);
+	shapes.push_back(boat);
+	shapes.push_back(blinker);
+	shapes.push_back(beacon);
+	shapes.push_back(pulsar);
+	shapes.push_back(pentadecathlon);
+	shapes.push_back(glider);
 
-	Pentadecathlon *pentadecathlon = new Pentadecathlon(5, 5);
-
-	Glider *glider = new Glider(2,2);
-	world.draw(glider);
-	
-	
+	for(auto shape : shapes){
+		world.draw(shape);
+	}
 	
 	world.play(animation);
 	return 0;
